a2a8e054f89b3f70f0f33c826af711375c498307
Mendel Konikov <mkonikov@gmail.com>
2017-08-22 17:44:02 -0400

    Correct Status Code

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
       login(@user)
       render "api/users/show"
     else
-      render :json ["Invalid Credentials"], status: 3
+      render :json ["Invalid Credentials"], status: 401
     end
   end
 
