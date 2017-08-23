22fc8c4f7e96ec3d97b191370be88f66655f858e
Mendel Konikov <mkonikov@gmail.com>
2017-08-23 17:44:26 -0400

    Add Upload Modal

---
+++ frontend/components/navbar/upload_modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class UploadModal extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <form>
+          Hi
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default UploadModal;
