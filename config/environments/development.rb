970cc5fb98a4d99ec13f4c7eafc9f2bc94ac7855
Mendel Konikov <mkonikov@gmail.com>
2017-08-28 09:19:42 -0400

    Remove Extra Bullet configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,13 +57,7 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
-    Bullet.honeybadger = true
-    Bullet.bugsnag = true
-    Bullet.airbrake = true
-    Bullet.rollbar = true
     Bullet.add_footer = true
-    Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
-    Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware' ]
   end
 
 end
