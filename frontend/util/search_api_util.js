4bd9b4ef2ce8e2b4d9b36a750b36250939f5fcd5
Mendel Konikov <mkonikov@gmail.com>
2017-08-31 14:48:28 -0400

    Minor fix

--- frontend/util/search_api_util.js
+++ frontend/util/search_api_util.js
@@ -4,3 +4,10 @@ export const searchUsers = (query) => {
     url: `/api/users/search?user=${query}`
   });
 };
+
+export const fetchNewUserSuggestions = () => {
+  return $.ajax({
+    method: 'get',
+    url: `/api/users/search/?suggestions=new_user`
+  });
+};
