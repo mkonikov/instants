b6907ea941c02ccc4702257f03335913ac49d32b
Mendel Konikov <mkonikov@gmail.com>
2017-09-04 15:46:21 -0400

    Logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,7 +6,7 @@ const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
   const logger = require('redux-logger');
-  middlewares.push(logger.createLogger());
+  middlewares.push(logger);
 }
 
 const configureStore = (preloadedState = {}) => (
