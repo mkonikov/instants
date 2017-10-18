867601e7c7e3486e31924801e8cd1446808c47cc
Mendel Konikov <mkonikov@gmail.com>
2017-10-18 15:41:14 -0400

    Add User association tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:username) }
-  it { should validate_length_of(:password).is_at_least(8) }
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_length_of(:password).is_at_least(8) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_presence_of(:email) }
+  end
+
+  describe 'Associations' do
+    it { should have_many(:followers) }
+    it { should have_many(:followees) }
+    it { should have_many(:posts).dependent(:destroy) }
+    it { should have_many(:likes).dependent(:destroy) }
+    it { should have_many(:comments).dependent(:destroy) }
+  end
+
 end
