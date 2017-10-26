bdf0580a7614238699d45fd42160d3533395f89c
Mendel Konikov <mkonikov@gmail.com>
2017-10-26 16:27:06 -0400

    Stub post image upload in controller test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,11 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
+  config.before(:each) do
+    allow_any_instance_of(Paperclip::Attachment).to receive(:save).and_return(true)
+  end
+
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
