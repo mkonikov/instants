8b5fcccbe2ddce0836517a0767bb00c2300c703d
Mendel Konikov <mkonikov@gmail.com>
2019-11-11 23:17:45 -0500

    Bump deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,8 @@
 const path = require('path');
-var webpack = require("webpack");
-
-var plugins = [];
-var devPlugins = [];
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
 
 module.exports = {
   context: __dirname,
-  plugins: plugins,
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './frontend/instants.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
